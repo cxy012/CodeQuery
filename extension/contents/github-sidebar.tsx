739072e8acafce06a3aefbb3dbe9d2e59ad32002
ygw212 <wangygca@gmail.com>
2024-11-04 17:33:44 -0800

    Update ui

--- extension/contents/github-sidebar.tsx
+++ extension/contents/github-sidebar.tsx
@@ -103,11 +103,23 @@ const Sidebar = () => {
           theme === "dark" ? "dark" : "light"
         }`}>
         <div className="flex flex-col h-full">
+          <div className="ml-auto ">
+            <Button
+              className="p-2 fill-foreground"
+              variant="outline"
+              size="sm"
+              onClick={(e) => {
+                e.preventDefault()
+                setShown(false)
+              }}>
+              <Cross1Icon className="h-4 w-4" />
+            </Button>
+          </div>
           <div className="mb-2">
-            <div className="flex mb-4">
+            <div className="flex my-4">
               <Button
                 className={"flex-1 px-4 py-2 text-foreground"}
-                variant={activeAITab === "ask" ? "secondary" : "ghost"}
+                variant={activeAITab === "ask" ? "ghost" : "secondary"}
                 onClick={() => setActiveAITab("search")}>
                 Search with AI
               </Button>
@@ -120,19 +132,6 @@ const Sidebar = () => {
             </div>
             {activeAITab === "search" && <SearchAIPanel />}
             {activeAITab === "ask" && <AskAiPanel />}
-          </div>
-
-          <div className="absolute right-4 top-4 flex">
-            <Button
-              className="p-2 fill-foreground"
-              variant="outline"
-              size="sm"
-              onClick={(e) => {
-                e.preventDefault()
-                setShown(false)
-              }}>
-              <Cross1Icon className="h-4 w-4" />
-            </Button>
           </div>
         </div>
       </div>
