3f4a95db33c49737a990e68b84deff97394af8f1
ygw212 <wangygca@gmail.com>
2024-11-05 16:24:10 -0800

    Update route.ts

--- backend/src/app/api/generate-text/route.ts
+++ backend/src/app/api/generate-text/route.ts
@@ -1,42 +1,10 @@
 import { NextRequest, NextResponse } from "next/server";
-import { z } from "zod";
 import { createGoogleGenerativeAI } from "@ai-sdk/google";
-import { generateText, streamText } from "ai";
-import { headers } from "next/headers";
-
-// const schema = z.object({
-//   prompt: z.string(),
-//   modelType: z.enum(["flash", "pro"]),
-// });
-
-const PROMPT_TEMPLATE =
-  "Explain what the selected code does in simple terms under given code context if provided. Assume the audience is a beginner programmer who has just " +
-  "learned the language features and basic syntax. The response should be plaintext, no markdown syntax included.";
+import { streamText } from "ai";
 
 export async function POST(req: NextRequest) {
   try {
-    // Validate request body using zod
-    //const requestBody = await req.json();
     const { prompt }: { prompt: string } = await req.json();
-    // const response = schema.safeParse(requestBody);
-
-    // if (!response.success) {
-    //   const { errors } = response.error;
-    //   return NextResponse.json(
-    //     { error: { message: "Invalid request params", errors } },
-    //     {
-    //       status: 400,
-    //       headers: {
-    //         "Access-Control-Allow-Origin": "*",
-    //         "Access-Control-Allow-Methods": "POST, OPTIONS",
-    //         "Access-Control-Allow-Headers": "Content-Type",
-    //       },
-    //     }
-    //   );
-    // }
-
-    //const { prompt: codeSnippet, modelType } = response.data;
-
     const apiKey = process.env.GOOGLE_AI_API_KEY;
     if (!apiKey) {
       return NextResponse.json(
@@ -58,33 +26,18 @@ export async function POST(req: NextRequest) {
 
     const flashModelId = "models/gemini-1.5-flash";
     const proModelId = "models/gemini-1.5-pro";
-    // const modelId = modelType === "flash" ? flashModelId : proModelId;
-
-    // const fullPrompt = `${PROMPT_TEMPLATE}\n\n${codeSnippet}`;
 
     const google = createGoogleGenerativeAI({ apiKey });
     const model = google(flashModelId);
 
-    // const { text } = await generateText({ model, prompt: fullPrompt });
-
-    // return NextResponse.json(
-    //   { text },
-    //   {
-    //     status: 200,
-    //     headers: {
-    //       "Access-Control-Allow-Origin": "*",
-    //       "Access-Control-Allow-Methods": "POST, OPTIONS",
-    //       "Access-Control-Allow-Headers": "Content-Type",
-    //     },
-    //   }
-    // );
     const stream = await streamText({ model, prompt: prompt });
     const origin = req.headers.get("origin") ?? "*";
     return stream.toDataStreamResponse({
       headers: {
         "Access-Control-Allow-Origin": origin,
         "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS",
-        "Access-Control-Allow-Headers":"Content-Type, Authorization, access-control-allow-origin",
+        "Access-Control-Allow-Headers":
+          "Content-Type, Authorization, access-control-allow-origin",
         "Access-Control-Allow-Credentials": "true",
       },
     });
