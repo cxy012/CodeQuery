4f6f22d04e7698528da9357a2aaf473dcea1bb72
ygw212 <wangygca@gmail.com>
2024-11-12 00:53:29 -0800

    update the display of completion

--- extension/components/ask-ai-panel.tsx
+++ extension/components/ask-ai-panel.tsx
@@ -43,7 +43,9 @@ export default function AskAIPanel() {
     handleInputChange,
     handleSubmit,
     isLoading,
-    setInput
+    setInput,
+    error,
+    setCompletion
   } = useCompletion({
     api: `${process.env.PLASMO_PUBLIC_BACKEND_URL}api/generate-text?modelType=${modelType}`
   })
@@ -114,172 +116,182 @@ export default function AskAIPanel() {
       )}
       <>
         <div className="mx-auto p-4 mb-2 ml-1 space-y-4 overflow-auto">
-          <div className="flex gap-2 justify-start">
-            <Badge
-              className={`cursor-pointer ${modelType === "gemini-1.5-flash-latest" ? "bg-blue-500 text-white" : "bg-gray-200 text-black"}`}
-              onClick={() => setModelType("gemini-1.5-flash-latest")}>
-              Gemini 1.5 Flash
-            </Badge>
-            <Badge
-              className={`cursor-pointer ${modelType === "gemini-1.5-pro-latest" ? "bg-blue-500 text-white" : "bg-gray-200 text-black"}`}
-              onClick={() => setModelType("gemini-1.5-pro-latest")}>
-              Gemini 1.5 Pro
-            </Badge>
-          </div>
-
-          <div className="flex flex-col gap-1.5 mt-4">
-            <div className="flex justify-between">
-              <Label htmlFor="prompt">Prompt</Label>
-              <Button
-                onClick={(e) => {
-                  e.preventDefault()
-                  handleCopy()
-                }}
-                variant="ghost"
-                className="p-2 h-3 justify-end">
-                <Clipboard className="h-3 w-3" />
-              </Button>
-            </div>
-            <div className="text-xs text-gray-500">
-              {promptInput.length} characters
-            </div>
-            <Textarea
-              className="text-xs overflow-hidden"
-              id="prompt"
-              onKeyDown={(e) => e.stopPropagation()}
-              value={promptInput}
-              onChange={(e) => setPromptInput(e.target.value)}
-            />
-          </div>
-
-          {/* select code for AI */}
-          <div>
-            <div className="flex flex-col mt-2">
-              <div className="flex items-center justify-between">
-                <Label>Target Code</Label>
-                <Button
-                  onClick={() => {
-                    setCodeSelectMode("code")
-                    setSelectedCodeForAI(undefined)
-                  }}
-                  disabled={codeSelectMode === "code" || isPrivate}
-                  variant="outline"
-                  size="xs">
-                  Select
-                </Button>
+          {!completion && (
+            <div>
+              <div className="flex gap-2 justify-start">
+                <Badge
+                  className={`cursor-pointer ${modelType === "gemini-1.5-flash-latest" ? "bg-blue-500 text-white" : "bg-gray-200 text-black"}`}
+                  onClick={() => setModelType("gemini-1.5-flash-latest")}>
+                  Gemini 1.5 Flash
+                </Badge>
+                <Badge
+                  className={`cursor-pointer ${modelType === "gemini-1.5-pro-latest" ? "bg-blue-500 text-white" : "bg-gray-200 text-black"}`}
+                  onClick={() => setModelType("gemini-1.5-pro-latest")}>
+                  Gemini 1.5 Pro
+                </Badge>
               </div>
-              <p className="text-xs text-gray-500 mt-2 mb-2">
-                Click [Select] to choose code for AI analysis.
-              </p>
-            </div>
-            <div
-              className="flex h-auto w-full px-3 py-2 rounded-md border border-input shadow-sm text-xs cursor-not-allowed opacity-50"
-              id="selected-code">
-              <pre className="whitespace-pre-wrap font-sans">
-                {selectedCodeForAI
-                  ? `${selectedCodeForAI.filePath}:${selectedCodeForAI.lineStart}-${selectedCodeForAI.lineEnd}`
-                  : "line_range"}
-              </pre>
-            </div>
 
-            <div className="flex flex-col gap-1.5 mt-2">
-              <Textarea
-                className="text-xs overflow-hidden font-sans"
-                value={selectedCodeForAI?.code || "Code content for AI..."}
-                disabled
-                readOnly
-              />
-            </div>
-          </div>
-
-          {/* select code context for AI */}
-          <div>
-            <div className="flex flex-col mt-2">
-              <div className="flex items-center justify-between">
-                <Label>Context Code</Label>
-                <Button
-                  onClick={() => setCodeSelectMode("context")}
-                  disabled={codeSelectMode === "context" || isPrivate}
-                  variant="outline"
-                  size="xs">
-                  Select
-                </Button>
-              </div>
-              <p className="text-xs text-gray-500 mt-2 mb-2">
-                Click [Select] to identify the relevant context code for AI
-                analysis.
-              </p>
-            </div>
-
-            {selectedCodeContextForAI.length > 0 ? (
-              selectedCodeContextForAI.map((code) => (
-                <div className="flex h-auto w-full px-3 py-2 rounded-md border border-input shadow-sm text-xs cursor-not-allowed opacity-50">
-                  <pre className="whitespace-pre-wrap">{`${code.filePath}:${code.lineStart}-${code.lineEnd}`}</pre>
+              <div className="flex flex-col gap-1.5 mt-4">
+                <div className="flex justify-between">
+                  <Label htmlFor="prompt">Prompt</Label>
                   <Button
-                    onClick={() =>
-                      handleDeleteContextCode(
-                        code.lineStart,
-                        code.lineEnd,
-                        code.filePath
-                      )
-                    }
-                    size="xs"
-                    variant="ghost">
-                    <Trash className="h-3 w-3" />
+                    onClick={(e) => {
+                      e.preventDefault()
+                      handleCopy()
+                    }}
+                    variant="ghost"
+                    className="p-2 h-3 justify-end">
+                    <Clipboard className="h-3 w-3" />
                   </Button>
                 </div>
-              ))
-            ) : (
-              <div className="flex h-auto w-full px-3 py-2 rounded-md border border-input shadow-sm text-xs cursor-not-allowed opacity-50">
-                <pre className="whitespace-pre-wrap">line_range</pre>
+                <div className="text-xs text-gray-500">
+                  {promptInput.length} characters
+                </div>
+                <Textarea
+                  className="text-xs overflow-hidden"
+                  id="prompt"
+                  onKeyDown={(e) => e.stopPropagation()}
+                  value={promptInput}
+                  onChange={(e) => setPromptInput(e.target.value)}
+                />
               </div>
-            )}
-          </div>
 
-          <div className="flex mt-4 justify-end gap-3">
-            <Button
-              onClick={(e) => {
-                e.preventDefault()
-                setCodeSelectMode(undefined)
-                setSelectedCodeForAI(undefined)
-                setSelectedCodeContextForAI([])
-              }}
-              size="sm"
-              variant="secondary">
-              Reset
-            </Button>
-            {isLoading ? (
-              <Button size="sm" disabled>
-                <Loader2 className="mr-2 h-3 w-3 animate-spin" />
-                Please wait
-              </Button>
-            ) : (
-              <Button
-                onClick={(e) => {
-                  e.preventDefault()
-                  if (
-                    !selectedCodeForAI &&
-                    selectedCodeContextForAI.length === 0
-                  ) {
-                    setStatusMessage(
-                      "Please ensure that selected code are provided."
-                    )
-                    return
-                  }
-                  handleSubmit()
-                }}
-                size="sm"
-                variant="outline"
-                className="bg-foreground text-background"
-                disabled={isPrivate}>
-                Send
-              </Button>
-            )}
-          </div>
+              {/* select code for AI */}
+              <div>
+                <div className="flex flex-col mt-2">
+                  <div className="flex items-center justify-between">
+                    <Label>Target Code</Label>
+                    <Button
+                      onClick={() => {
+                        setCodeSelectMode("code")
+                        setSelectedCodeForAI(undefined)
+                      }}
+                      disabled={codeSelectMode === "code" || isPrivate}
+                      variant="outline"
+                      size="xs">
+                      Select
+                    </Button>
+                  </div>
+                  <p className="text-xs text-gray-500 mt-2 mb-2">
+                    Click [Select] to choose code for AI analysis.
+                  </p>
+                </div>
+                <div
+                  className="flex h-auto w-full px-3 py-2 rounded-md border border-input shadow-sm text-xs cursor-not-allowed opacity-50"
+                  id="selected-code">
+                  <pre className="whitespace-pre-wrap font-sans">
+                    {selectedCodeForAI
+                      ? `${selectedCodeForAI.filePath}:${selectedCodeForAI.lineStart}-${selectedCodeForAI.lineEnd}`
+                      : "line_range"}
+                  </pre>
+                </div>
 
+                <div className="flex flex-col gap-1.5 mt-2">
+                  <Textarea
+                    className="text-xs overflow-hidden font-sans"
+                    value={selectedCodeForAI?.code || "Code content for AI..."}
+                    disabled
+                    readOnly
+                  />
+                </div>
+              </div>
+
+              {/* select code context for AI */}
+              <div>
+                <div className="flex flex-col mt-2">
+                  <div className="flex items-center justify-between">
+                    <Label>Context Code</Label>
+                    <Button
+                      onClick={() => setCodeSelectMode("context")}
+                      disabled={codeSelectMode === "context" || isPrivate}
+                      variant="outline"
+                      size="xs">
+                      Select
+                    </Button>
+                  </div>
+                  <p className="text-xs text-gray-500 mt-2 mb-2">
+                    Click [Select] to identify the relevant context code for AI
+                    analysis.
+                  </p>
+                </div>
+
+                {selectedCodeContextForAI.length > 0 ? (
+                  selectedCodeContextForAI.map((code) => (
+                    <div className="flex h-auto w-full px-3 py-2 rounded-md border border-input shadow-sm text-xs cursor-not-allowed opacity-50">
+                      <pre className="whitespace-pre-wrap">{`${code.filePath}:${code.lineStart}-${code.lineEnd}`}</pre>
+                      <Button
+                        onClick={() =>
+                          handleDeleteContextCode(
+                            code.lineStart,
+                            code.lineEnd,
+                            code.filePath
+                          )
+                        }
+                        size="xs"
+                        variant="ghost">
+                        <Trash className="h-3 w-3" />
+                      </Button>
+                    </div>
+                  ))
+                ) : (
+                  <div className="flex h-auto w-full px-3 py-2 rounded-md border border-input shadow-sm text-xs cursor-not-allowed opacity-50">
+                    <pre className="whitespace-pre-wrap">line_range</pre>
+                  </div>
+                )}
+              </div>
+
+              <div className="flex mt-4 justify-end gap-3">
+                <Button
+                  onClick={(e) => {
+                    e.preventDefault()
+                    setCodeSelectMode(undefined)
+                    setSelectedCodeForAI(undefined)
+                    setSelectedCodeContextForAI([])
+                  }}
+                  size="sm"
+                  variant="secondary">
+                  Reset
+                </Button>
+                {isLoading ? (
+                  <Button size="sm" disabled>
+                    <Loader2 className="mr-2 h-3 w-3 animate-spin" />
+                    Please wait
+                  </Button>
+                ) : (
+                  <Button
+                    onClick={(e) => {
+                      e.preventDefault()
+                      if (
+                        !selectedCodeForAI &&
+                        selectedCodeContextForAI.length === 0
+                      ) {
+                        setStatusMessage(
+                          "Please ensure that selected code are provided."
+                        )
+                        return
+                      }
+                      handleSubmit()
+                    }}
+                    size="sm"
+                    variant="outline"
+                    className="bg-foreground text-background"
+                    disabled={isPrivate}>
+                    Send
+                  </Button>
+                )}
+              </div>
+            </div>
+          )}
+          {error && <div className="text-red-500">{error.message}</div>}
           {completion && (
             <div className="my-4">
-              {/* <Textarea value={completion} rows={8} readOnly /> */}
+              <Button
+                size="sm"
+                variant="outline"
+                className="bg-foreground text-background mb-2"
+                onClick={() => setCompletion(null)}>
+                back
+              </Button>
               <ReactMarkdown
                 remarkPlugins={[remarkGfm]}
                 components={{
